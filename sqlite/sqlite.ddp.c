64e111775908a1218f3acfb6053f656a8aa4f084
bafto <hendrikziegler@outlook.de>
2024-08-10 16:08:07 +0200

    sqlite

--- sqlite/sqlite.ddp.c
+++ sqlite/sqlite.ddp.c
@@ -1,8 +1,12 @@
 #include "DDP/ddpmemory.h"
 #include "DDP/ddptypes.h"
+#include "DDP/error.h"
 #include "lib/sqlite3.h"
+#include <assert.h>
 #include <stdio.h>
 #include <string.h>
+
+static_assert(sizeof(int) == sizeof(ddpchar), "int must be 4 bytes");
 
 typedef ddpint Zeiger;
 typedef ddpint *ZeigerRef;
@@ -13,32 +17,33 @@ typedef ZeigerRef DatenbankRef;
 typedef Zeiger SQLAusdruck;
 typedef ZeigerRef SQLAusdruckRef;
 
-ddpbool Oeffne_DB(DatenbankRef p, ddpstring *db_name) {
+void Oeffne_DB(DatenbankRef p, ddpstring *db_name) {
 	struct sqlite3 **db = (struct sqlite3 **)p;
 	int err = sqlite3_open(db_name->str, db);
 	if (err != SQLITE_OK) {
-		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(*db));
+		ddp_error("Datenbank '%s' konnte nicht geöffnet werden: %s\n",
+				  false, db_name->str, sqlite3_errmsg(*db));
 		sqlite3_close(*db);
-		return false;
 	}
-	return true;
 }
 
 void Schließe_DB(Datenbank db) {
-	sqlite3_close((struct sqlite3 *)db);
+	int err = sqlite3_close((struct sqlite3 *)db);
+	if (err != SQLITE_OK) {
+		ddp_error("Datenbank konnte nicht geschlossen werden: %s\n",
+				  false, sqlite3_errmsg((struct sqlite3 *)db));
+	}
 }
 
-ddpbool Statement_Vorbereiten(Datenbank pDB, SQLAusdruckRef pStmt,
-							  ddpstring *sql) {
+void Statement_Vorbereiten(Datenbank pDB, SQLAusdruckRef pStmt,
+						   ddpstring *sql) {
 	struct sqlite3 *db = (struct sqlite3 *)pDB;
 	struct sqlite3_stmt **stmt = (struct sqlite3_stmt **)pStmt;
 	int err = sqlite3_prepare_v2(db, sql->str, -1, stmt, NULL);
 	if (err != SQLITE_OK) {
-		fprintf(stderr, "Error in preparing statement: %s\n", sqlite3_errmsg(db));
-		return false;
+		ddp_error("Fehler beim Vorbereiten des Statements: %s\n",
+				  false, sqlite3_errmsg(db));
 	}
-
-	return true;
 }
 
 void Statement_Schließen(SQLAusdruck stmt) {
@@ -46,7 +51,38 @@ void Statement_Schließen(SQLAusdruck stmt) {
 }
 
 ddpbool Naechste_Zeile_Vorbereiten(SQLAusdruck stmt) {
-	return sqlite3_step((struct sqlite3_stmt *)stmt) == SQLITE_ROW;
+	int err = sqlite3_step((struct sqlite3_stmt *)stmt) == SQLITE_ROW;
+	if (err != SQLITE_DONE && err != SQLITE_ROW) {
+		ddp_error("Fehler beim Ausführen des Statements: %s\n",
+				  false, sqlite3_errmsg(sqlite3_db_handle((struct sqlite3_stmt *)stmt)));
+	}
+	return err == SQLITE_ROW;
+}
+
+ddpbool War_NULL(SQLAusdruck stmt, ddpint spalte) {
+	return sqlite3_column_type((struct sqlite3_stmt *)stmt, spalte) == SQLITE_NULL;
+}
+
+void Lies_Spalte_Text(ddpstring *ret, SQLAusdruck stmt, ddpint spalte) {
+	const unsigned char *text = sqlite3_column_text((struct sqlite3_stmt *)stmt, spalte);
+	*ret = DDP_EMPTY_STRING;
+
+	if (!text) {
+		return;
+	}
+
+	int len = strlen((const char *)text);
+	ret->cap = len + 1;
+	ret->str = DDP_ALLOCATE(char, ret->cap);
+	memcpy(ret->str, text, len);
+}
+
+ddpint Lies_Spalte_Zahl(SQLAusdruck stmt, ddpint spalte) {
+	return sqlite3_column_int64((struct sqlite3_stmt *)stmt, spalte);
+}
+
+ddpfloat Lies_Spalte_Kommazahl(SQLAusdruck stmt, ddpint spalte) {
+	return sqlite3_column_double((struct sqlite3_stmt *)stmt, spalte);
 }
 
 void Lies_Text(ddpstring *ret, SQLAusdruck stmt) {
