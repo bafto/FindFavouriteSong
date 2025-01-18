0995d7897042c3efdff1773f195388e8be429b72
Hendrik Ziegler <hendrikziegler@outlook.de>
2025-01-18 13:25:37 +0100

    users and passwords can now be passed as csv via environment variable

--- config.go
+++ config.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log/slog"
+	"strings"
 	"time"
 
 	"github.com/spf13/viper"
@@ -33,7 +35,7 @@ func read_config() (Config, error) {
 	viper.SetDefault("checkpoint_interval", 2*time.Hour)
 	viper.SetDefault("checkpoint_interval", 1*time.Minute)
 
-	viper.SetEnvPrefix("ffs")
+	viper.SetEnvPrefix("FFS")
 	viper.AutomaticEnv()
 
 	viper.AddConfigPath(".")
@@ -44,7 +46,31 @@ func read_config() (Config, error) {
 		panic(err)
 	}
 
+	// users are read from environment if present
+	viper.BindEnv("users")
+
+	// check if users is set via env
+	usersMapEnv := viper.GetString("users")
+	if usersMapEnv != "" {
+		// If environment variable is set, handle parsing
+		slog.Info("Using environment variable for users")
+		users := parseCommaSeparatedMap(usersMapEnv)
+		viper.Set("users", users)
+	}
+
 	var config Config
 	err := viper.Unmarshal(&config)
 	return config, err
+}
+
+func parseCommaSeparatedMap(input string) map[string]string {
+	result := make(map[string]string)
+	pairs := strings.Split(input, ",")
+	for _, pair := range pairs {
+		kv := strings.SplitN(pair, "=", 2)
+		if len(kv) == 2 {
+			result[kv[0]] = kv[1]
+		}
+	}
+	return result
 }
